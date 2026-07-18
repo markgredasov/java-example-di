9f15038e57435539fc2b133963b4d2eb36835e26
markgredasov <markgredasov@icloud.com>
2026-07-18 22:25:47 +0300

    added requests to internal service

--- src/main/java/com/example/demo/ReservationController.java
+++ src/main/java/com/example/demo/ReservationController.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -11,6 +15,10 @@ public class ReservationController {
 
   private final ReservationService reservationService;
   private static final Logger log = LoggerFactory.getLogger(ReservationController.class);
+
+  @Autowired
+  private RestTemplate restTemplate;
+
 
   public ReservationController(ReservationService reservationService) {
     this.reservationService = reservationService;
@@ -29,14 +37,63 @@ public class ReservationController {
   }
 
   @PostMapping("/reservations")
-  public Reservation createReservation(@RequestBody Reservation reservationToCreate) {
+  public Reservation createReservation(@RequestBody Reservation reservationToCreate, HttpServletRequest request) {
     log.info("create reservation called");
-    return reservationService.createReservation(reservationToCreate);
+
+    Reservation createdReservation = reservationService.createReservation(reservationToCreate);
+
+    String traceID = getTraceIdFromRequest(request);
+
+    sendToInternalService(createdReservation, traceID);
+
+    return createdReservation;
   }
 
   @DeleteMapping("/reservations/{id}")
   public Reservation deleteReservationByID(@PathVariable("id") Long id) {
     log.info("delete reservation by id called");
     return reservationService.deleteReservationByID(id);
+  }
+
+  private String getTraceIdFromRequest(HttpServletRequest request) {
+    String traceID = (String) request.getAttribute("traceID");
+    return traceID == null ? "" : traceID;
+  }
+
+  private void sendToInternalService(Reservation reservation, String traceID) {
+    try {
+      String internalServiceURL = getInternalServiceURL();
+
+      var headers = getHttpHeaders(traceID);
+
+      HttpEntity<Reservation> request = new HttpEntity<>(reservation, headers);
+      ResponseEntity<String> response = restTemplate.exchange(internalServiceURL, HttpMethod.POST, request, String.class);
+
+      LogSendToInternalServiceResult(response);
+    } catch (Exception e) {
+      log.error("failed to send request to internal service", e);
+    }
+  }
+
+  private String getInternalServiceURL() {
+    return "http://localhost:8081/internal/log";
+  }
+
+  private HttpHeaders getHttpHeaders(String traceID) {
+    final String TRACE_ID_HEADER = "X-Trace-ID";
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.set(TRACE_ID_HEADER, traceID);
+    headers.setContentType(MediaType.APPLICATION_JSON);
+
+    return headers;
+  }
+
+  private void LogSendToInternalServiceResult(ResponseEntity<String> response) {
+    if (response.getStatusCode().is2xxSuccessful()) {
+      log.info("reservation sent to internal service successfully | response: {}", response.getBody());
+    } else {
+      log.warn("internal service returned: {}", response.getStatusCode());
+    }
   }
 }
--- src/main/java/com/example/demo/TraceInterceptor.java
+++ src/main/java/com/example/demo/TraceInterceptor.java
@@ -12,6 +12,8 @@ import java.util.UUID;
 
 @Component
 public class TraceInterceptor implements HandlerInterceptor {
+  private static final String TRACE_ID_HEADER = "X-Trace-ID";
+
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
     String traceID = UUID.randomUUID().toString();
@@ -19,9 +21,10 @@ public class TraceInterceptor implements HandlerInterceptor {
     HttpServletRequest requestWithHeaders = new HttpServletRequestWrapper(request) {
       private final Map<String, String> headers = new HashMap<>();
 
-
       {
-        headers.put("X-Trace-ID", traceID);
+        if (headers.get(TRACE_ID_HEADER) == null) {
+          headers.put(TRACE_ID_HEADER, traceID);
+        };
       }
 
       @Override
